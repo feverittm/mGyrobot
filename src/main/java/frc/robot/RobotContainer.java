e49ff1e6b54c6ac3e8921d42852c5902d71b6c6d
Floyd Moore <floyd.moore@hp.com>
2022-02-01 22:31:15 -0800

    getting climber methods set up

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -22,91 +22,99 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.POVButton;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems
-  private final DriveSubsystem m_robotDrive = new DriveSubsystem();
-  private final ClimberSubsystem m_climber = new ClimberSubsystem();
+    // The robot's subsystems
+    private final DriveSubsystem m_robotDrive = new DriveSubsystem();
+    private final ClimberSubsystem m_climber = new ClimberSubsystem();
 
-  // The driver's controller
-  XboxController m_driverController = new XboxController(OIConstants.kDriverControllerPort);
+    // The driver's controller
+    XboxController m_driverController = new XboxController(OIConstants.kDriverControllerPort);
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
-    // Configure the button bindings
-    configureButtonBindings();
+    /**
+     * The container for the robot. Contains subsystems, OI devices, and commands.
+     */
+    public RobotContainer() {
+        // Configure the button bindings
+        configureButtonBindings();
 
-    // Configure default commands
-    // Set the default drive command to split-stick arcade drive
-    m_robotDrive.setDefaultCommand(
-        // A split-stick arcade command, with forward/backward controlled by the left
-        // hand, and turning controlled by the right.
-        new RunCommand(
-            () ->
-                m_robotDrive.arcadeDrive(
-                    m_driverController.getLeftY(), m_driverController.getRightX()),
-            m_robotDrive));
-  }
+        // Configure default commands
+        // Set the default drive command to split-stick arcade drive
+        m_robotDrive.setDefaultCommand(
+                // A split-stick arcade command, with forward/backward controlled by the left
+                // hand, and turning controlled by the right.
+                new RunCommand(
+                        () -> m_robotDrive.arcadeDrive(
+                                m_driverController.getLeftY(), m_driverController.getRightX()),
+                        m_robotDrive));
+    }
 
-  /**
-   * Use this method to define your button->command mappings. Buttons can be created by
-   * instantiating a {@link edu.wpi.first.wpilibj.GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link PS4Controller}), and then passing it to a {@link
-   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-   */
-  private void configureButtonBindings() {
-    // Drive at half speed when the right bumper is held
-    new JoystickButton(m_driverController, Button.kRightBumper.value)
-        .whenPressed(() -> m_robotDrive.setMaxOutput(0.5))
-        .whenReleased(() -> m_robotDrive.setMaxOutput(1));
+    /**
+     * Use this method to define your button->command mappings. Buttons can be
+     * created by
+     * instantiating a {@link edu.wpi.first.wpilibj.GenericHID} or one of its
+     * subclasses ({@link
+     * edu.wpi.first.wpilibj.Joystick} or {@link PS4Controller}), and then passing
+     * it to a {@link
+     * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+     */
+    private void configureButtonBindings() {
+        // Drive at half speed when the right bumper is held
+        new JoystickButton(m_driverController, Button.kRightBumper.value)
+                .whenPressed(() -> m_robotDrive.setMaxOutput(0.5))
+                .whenReleased(() -> m_robotDrive.setMaxOutput(1));
 
-    // Stabilize robot to drive straight with gyro when left bumper is held
-    new JoystickButton(m_driverController, Button.kLeftBumper.value)
-        .whenHeld(
-            new PIDCommand(
-                new PIDController(
-                    DriveConstants.kStabilizationP,
-                    DriveConstants.kStabilizationI,
-                    DriveConstants.kStabilizationD),
-                // Close the loop on the turn rate
-                m_robotDrive::getTurnRate,
-                // Setpoint is 0
-                0,
-                // Pipe the output to the turning controls
-                output -> m_robotDrive.arcadeDrive(m_driverController.getLeftY(), output),
-                // Require the robot drive
-                m_robotDrive));
+        // Stabilize robot to drive straight with gyro when left bumper is held
+        new JoystickButton(m_driverController, Button.kLeftBumper.value)
+                .whenHeld(
+                        new PIDCommand(
+                                new PIDController(
+                                        DriveConstants.kStabilizationP,
+                                        DriveConstants.kStabilizationI,
+                                        DriveConstants.kStabilizationD),
+                                // Close the loop on the turn rate
+                                m_robotDrive::getTurnRate,
+                                // Setpoint is 0
+                                0,
+                                // Pipe the output to the turning controls
+                                output -> m_robotDrive.arcadeDrive(m_driverController.getLeftY(), output),
+                                // Require the robot drive
+                                m_robotDrive));
 
-    // Turn to 90 degrees when the 'X' button is pressed, with a 5 second timeout
-    new JoystickButton(m_driverController, Button.kB.value)
-        .whenPressed(new TurnToAngleProfiled(90, m_robotDrive).withTimeout(5));
+        // Turn to 90 degrees when the 'X' button is pressed, with a 5 second timeout
+        new JoystickButton(m_driverController, Button.kB.value)
+                .whenPressed(new TurnToAngleProfiled(90, m_robotDrive).withTimeout(5));
 
-    new JoystickButton(m_driverController, Button.kX.value)
-        .whenPressed(new DriveToDistance(48, m_robotDrive).withTimeout(5));
+        new JoystickButton(m_driverController, Button.kX.value)
+                .whenPressed(new DriveToDistance(48, m_robotDrive).withTimeout(5));
 
-    // Turn to -90 degrees with a profile when the Circle button is pressed, with a 5 second timeout
-    new JoystickButton(m_driverController, Button.kX.value)
-        .whenPressed(new TurnToAngleProfiled(-90, m_robotDrive).withTimeout(5));
+        // Turn to -90 degrees with a profile when the Circle button is pressed, with a
+        // 5 second timeout
+        new JoystickButton(m_driverController, Button.kX.value)
+                .whenPressed(new TurnToAngleProfiled(-90, m_robotDrive).withTimeout(5));
 
-    new JoystickButton(m_driverController, Button.kY.value)
-        .whenPressed(() -> m_robotDrive.resetRobot());
+        new JoystickButton(m_driverController, Button.kY.value)
+                .whenPressed(() -> m_robotDrive.resetRobot());
 
-    new POVButton(m_driverController, 0)
-        .whenPressed(() -> m_climber.climb(0.2))
-        .whenReleased(() -> m_climber.climb(0));
-  }
+        new POVButton(m_driverController, 0)
+                .whenPressed(() -> m_climber.climb(0.2))
+                .whenReleased(() -> m_climber.climb(0));
+    }
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    // no auto
-    return new InstantCommand();
-  }
+    /**
+     * Use this to pass the autonomous command to the main {@link Robot} class.
+     *
+     * @return the command to run in autonomous
+     */
+    public Command getAutonomousCommand() {
+        // no auto
+        return new InstantCommand();
+    }
 }
